49ef2ded81bce66c1b8958630697c45a6420890d
Kris Longmore <kris@robotwealth.com>
2022-05-03 15:48:00 +0800

    add comments and questions

--- src/roq/samples/zeromq/strategy.cpp
+++ src/roq/samples/zeromq/strategy.cpp
@@ -22,12 +22,19 @@ auto create_socket(auto &context) {
 }
 }  // namespace
 
+// initialise a strategy with a dispatcher and a socket (created from a context)
 Strategy::Strategy(client::Dispatcher &dispatcher) : dispatcher_(dispatcher), socket_(create_socket(context_)) {
 }
 
+// implement the TopOfBook Event type operator
 void Strategy::operator()(const Event<TopOfBook> &event) {
-  auto &[message_info, top_of_book] = event;
-  auto &layer = top_of_book.layer;
+  auto &[message_info, top_of_book] = event;  // deconstruct message into its two parts (message_info and top_of_book)
+  /*
+   Example message:
+    message_info={source=0, source_name="ftx", source_session_id="a2f73b39-dd97-45bf-842d-2be9a05b0c4a", source_seqno=31836, receive_time_utc=1651113981345409265ns, receive_time=482186573715285ns, source_send_time=482186573715285ns, source_receive_time=482186573136443ns, origin_create_time=482186573136443ns, origin_create_time_utc=1651113981344830599ns, is_last=true, opaque=0},
+    top_of_book={stream_id=6, exchange="ftx", symbol="BTC/USD", layer={bid_price=39449, bid_quantity=0.5257000000000001, ask_price=39450, ask_quantity=8.0679}, update_type=INCREMENTAL, exchange_time_utc=1651113981333000000ns}
+  */
+  auto &layer = top_of_book.layer;  // note layer object in top_of_book message example above
   // json message
   // note! you can optimize this by pre-allocating a buffer and use fmt::format_to
   auto message = fmt::format(
@@ -46,3 +53,10 @@ void Strategy::operator()(const Event<TopOfBook> &event) {
 }  // namespace zeromq
 }  // namespace samples
 }  // namespace roq
+
+/* QUESTIONS
+ * I don't understand Strategy initialisation in enough detail - syntax issue.
+ * Where do I create the Dispatcher instance with which the Strategy is initialised? At the call site?
+ * Need to check we're using appropriate ZMQ queues and socket types
+
+*/
--- src/roq/samples/zeromq/strategy.hpp
+++ src/roq/samples/zeromq/strategy.hpp
@@ -17,18 +17,27 @@ namespace zeromq {
 
 // strategy implementation
 
+// final keyword means that Strategy can not be inherited from
 class Strategy final : public client::Handler {
+/*
+* Strategy inherits from client::Handler : https://roq-trading.com/docs/reference/development/roq-api/client/#handler
+* client::Handler is used to implement handlers for update events
+* Consists of a number of virtual public overloaded operator() functions that each take a different Event type
+* (a virtual function implemented in the base class can be overridden in the subclass. good practice to mark the overwritten function with keyword override)
+* Here, we implement the operator() function for handling TopOfBook Events
+ */
  public:
-  explicit Strategy(client::Dispatcher &);
+  // constructor - Strategy is initialised with a Dispatcher
+  explicit Strategy(client::Dispatcher &);  // explicit keyword disables implicit construction ie must be created with Strategy myStrategy(client::Dispatcher &) - bit of a safety feature
 
-  Strategy(Strategy &&) = default;
-  Strategy(const Strategy &) = delete;
+  Strategy(Strategy &&) = default;  // default constructor
+  Strategy(const Strategy &) = delete;  // remove this constructor
 
  protected:
   void operator()(const Event<TopOfBook> &) override;
 
  private:
-  client::Dispatcher &dispatcher_;
+  client::Dispatcher &dispatcher_;  // Dispatcher is used to manage client requests
   zmq::Context context_;
   zmq::Socket socket_;
 };
